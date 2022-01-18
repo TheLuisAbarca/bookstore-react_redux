24086d7ac343c1d07c278a7e271a9b38f46301a2
TheLuisAbarca <arclaav.adm@gmail.com>
2022-01-18 12:12:58 -0500

    Add Implementation of setup the basic structure of Project.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import routes from './routes';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <>
+      <Navbar />
+      <div className="Bookstore">
+        <Routes>
+          {routes.map(({ routePath, routeComponent }) => (
+            <Route key={routePath} path={routePath} element={routeComponent} />
+          ))}
+        </Routes>
+      </div>
+    </>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/BookElement.jsx
@@ -0,0 +1,17 @@
+const Book = () => (
+  <li>
+    <div>category</div>
+    <div>Title</div>
+    <div>Author</div>
+    <div>comments</div>
+    <button type="button">Remove</button>
+    <div>Edit</div>
+    <div>Percentage</div>
+    <div>completed</div>
+    <div>Current chapter</div>
+    <div>chapter number</div>
+    <button type="button">Update progress</button>
+  </li>
+);
+  
+export default Book;
